6b0fbe16df4cbb86a390f71605e08f9b7b02c683
r3llie <ghailstone503@gmail.com>
2022-01-22 13:01:53 -0800

    Made forecast for the next 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -11,15 +11,20 @@ export default function WeatherForcast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-  
 
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
